3c46c5b88e8af1985220b1cf4d3335baf1256bfd
Boev Aleksandr <b.0.e.v@yandex.ru>
2021-07-23 01:34:45 +0300

    feat: auth

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const NotFoundError = require('./errors/notfound-error');
 
-const { login, createUser } = require('./controllers/users');
-
 const { PORT = 3000 } = process.env;
+
 const app = express();
 
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -20,8 +21,30 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/^((http|https):\/\/)(www\.)?([\w\W\d]{1,})(\.)([a-zA-Z]{1,10})([\w\W\d]{1,})?$/),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }).unknown(true),
+}), createUser);
+
+app.use(auth);
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
@@ -30,6 +53,8 @@ app.all('*', (req, res) => {
   const ERROR = new NotFoundError('Ресурс не найден');
   res.status(ERROR.statusCode).send({ message: ERROR.message });
 });
+
+app.use(errors());
 
 app.use((error, req, res, next) => {
   const { statusCode = 500, message } = error;
--- controllers/users.js
+++ controllers/users.js
@@ -72,12 +72,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id },
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' });
-      res
-        .cookie('jwt', token, {
-          maxAge: 3600000,
-          httpOnly: true,
-        })
-        .end();
+      res.send({ token });
     })
     .catch(next);
 };
